3fdc62bcfb020772f1ae3e46211139f729380cbd
kaledguz <guzman.kaled.pro@gmail.com>
2023-11-25 21:31:19 +0100

    implementation du responsive 2
    ici je répare et je met en plac ele menu en responsive

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
       nav.classList.toggle('active');
     });
   });
+  // Sélectionnez le carrousel
+const carousel = document.querySelector('.carousel-container');
+// Sélectionnez le header/menu
+const header = document.querySelector('header');
+
+// Fonction pour gérer le changement de transparence du menu
+function handleScroll() {
+  const carouselBottom = carousel.getBoundingClientRect().bottom;
+
+  if (carouselBottom <= 0) {
+    // Ajouter la classe 'transparent-menu' lorsque le carrousel atteint le haut de la fenêtre
+    header.classList.add('transparent-menu');
+  } else {
+    // Supprimer la classe 'transparent-menu' lorsque le carrousel n'est pas visible en haut de la fenêtre
+    header.classList.remove('transparent-menu');
+  }
+}
+
+// Écoutez l'événement de scroll et appelez la fonction handleScroll
+window.addEventListener('scroll', handleScroll);
